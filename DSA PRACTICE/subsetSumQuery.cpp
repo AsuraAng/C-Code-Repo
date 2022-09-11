0f7f5f8acf434f89acfa295b60709659a89ef983
AsuraAng <100083709+AsuraAng@users.noreply.github.com>
2022-09-11 19:03:42 +0530

    Commiting the Modifications

--- DSA PRACTICE/subsetSumQuery.cpp
+++ DSA PRACTICE/subsetSumQuery.cpp
@@ -58,6 +58,40 @@ void checkEven(int a)
     else cout << "The Given Number is Even !!!"<<endl;
 }
 
+// Funcions for changing the bits
+int getIthBit(int n , int i)
+{
+    int mask = (1<<i);
+    return (n & mask) > ? 1 : 0;
+}
+
+int setIthBit(int &n , int i)
+{
+    int mask = ~(1<<i);
+    n = n&mask;
+}
+
+void updateIthBit(int &n , int i , int v)
+{
+    clearIthBit(n,i);
+    int mask = (v<<i);
+    n = n|mask;
+}
+
+int clearLastIthBitss(int &n ,int i)
+{
+    int mask = (-1<<i);
+    n = n & mask;
+}
+
+int clearBitsInRange(int &n ,int i ,int j)
+{
+    int a = (~0)<<(j+1);
+    int b = (1<<i)-1;
+    int mask = a | b;
+    n = n & mask;
+}
+
 int main()
 {
     int x ;
